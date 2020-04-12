15b953f5cbf57f08fc1adfabd96df19cb7e0d135
RedLyon1200 <sagudelo1200@gmail.com>
2020-04-12 01:25:39 -0500

    indentation code corrected

--- simple_shell_1.0/_getenv.c
+++ simple_shell_1.0/_getenv.c
@@ -1,15 +1,19 @@
 #include "header.h"
-
+/**
+ * _getenv - obtain the environment variable
+ *
+ * Return: 0 success
+ */
 void *_getenv()
 {
 	int i = 1;
 
 	char *s = *environ;
 
-  	for (; s; i++)
+	for (; s; i++)
 	{
 		printf("%s\n", s);
-		s = *(environ+i);
+		s = *(environ + i);
 	}
-	return 0;
+	return (0);
 }
