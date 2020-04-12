a978c14ac2f8d50c45a5ab09d41c1cff4f48867b
FatChicken277 <1637@holbertonschool.com>
2020-04-11 19:05:34 -0500

    Added v.01 & v0.1.1 project versions

---
+++ Simple_shell_0.1\0.1.1/_getline.c
@@ -0,0 +1,24 @@
+#include "header.h"
+/**
+ * _getline - get words.
+ * @buffer: buffer.
+ * Return: 0 if its ok, and -1 if its bad.
+ */
+int _getline(char buffer[])
+{
+	int i = 0, a;
+	char buf[1023];
+
+	a = read(STDIN_FILENO, buf, 1023);
+	if (a == 0 || buffer == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < a; i++)
+	{
+		buffer[i] = buf[i];
+	}
+	buffer[i] = '\0';
+	return (0);
+}
