8c7d8c5f1fd972adb9c67bb7e4dc4d335b30302c
FatChicken277 <1637@holbertonschool.com>
2020-04-14 19:00:01 -0500

    Removed prohibited functions

--- welcome.c
+++ welcome.c
@@ -4,14 +4,13 @@
  */
 void welcome(void)
 {
-	printf("\033[1;1H\033[2J");
-	printf("\033[1;31m");
-	printf(" _____ __    _____ _____ _____      ) \n");
-	printf("|   __|  |  |  _  |     |   __|    ) \\ \n");
-	printf("|   __|  |__|     | | | |   __|   / ) ( \n");
-	printf("|__|  |_____|__|__|_|_|_|_____|   \\(_)/ \n");
-	printf("________________________________________\n");
-	printf("\n");
-	printf("\033[m");
+	write(STDOUT_FILENO, "\033[1;1H\033[2J", 10);
+	write(STDOUT_FILENO, "\033[1;31m", 8);
+	write(STDOUT_FILENO, " _____ __    _____ _____ _____      ) \n", 40);
+	write(STDOUT_FILENO, "|   __|  |  |  _  |     |   __|    ) \\ \n", 41);
+	write(STDOUT_FILENO, "|   __|  |__|     | | | |   __|   / ) ( \n", 41);
+	write(STDOUT_FILENO, "|__|  |_____|__|__|_|_|_|_____|   \\(_)/ \n", 42);
+	write(STDOUT_FILENO, "________________________________________\n", 41);
+	write(STDOUT_FILENO, "\n", 1);
+	write(STDOUT_FILENO, "\033[m", 3);
 }
-
